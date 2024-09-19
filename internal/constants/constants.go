029668f409cf4c14d0e99dfb1a2ba2547ebf93c1
Angelo Costa <ang.costa.neto@gmail.com>
2024-09-19 13:12:13 -0300

    Add comments in portuguese

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,5 +1,10 @@
 package constants
 
+// Declara a constante TAX com o valor 0.2
 const TAX = 0.2
+
+// Declara a constante BuyOperation com o valor buy
 const BuyOperation = "buy"
+
+// Declara a contante SellOperation com o valor sell
 const SellOperation = "sell"
--- internal/models/model.go
+++ internal/models/model.go
@@ -1,17 +1,29 @@
 package models
 
+/**
+ * Declara a estrutura de dados para o transaction
+ * aqui usa a tag `json:"field-name"` para definir como os campos serializados/deserializados
+ * também pode usar a tag `xml:"field-name"` para mapear em xml
+ * quanto a struct for convertido para JSON e vice-versa
+ */
 type Transaction struct {
-	Code      string  `json:"code"`
-	Operation string  `json:"operation"`
-	UnitCost  float64 `json:"unit-cost"`
-	Quantity  int     `json:"quantity"`
+	Code      string  `json:"code"`      // O campo Code será mapeado para o campo "code" no JSON
+	Operation string  `json:"operation"` // O campo Operation será mapeado para o campo "operation" no JSON
+	UnitCost  float64 `json:"unit-cost"` // O campo UnitCost será mapeado para o campo "unit-cost" no JSON
+	Quantity  int     `json:"quantity"`  // O campo Quantity será mapeado para o campo "quantity" no JSON
 }
 
+/**
+ * Declara a estrutua de dados para a tax result
+ */
 type TaxResult struct {
-	Code string  `json:"code"`
-	Tax  float64 `json:"tax"`
+	Code string  `json:"code"` // O campo Code será mapeado para o campo "code" no JSON
+	Tax  float64 `json:"tax"`  // O campo Taz será mapeado para o campo "tax" no JSON
 }
 
+/**
+ * Declara a estrutura de dados para o operation
+ */
 type Operation struct {
 	Quantity             int
 	WeightedAveragePrice float64
